e55f5c180c87268611a83b8de6fbf15665b870de
Eshan <eshan.20221570@iit.ac.lk>
2024-03-17 11:35:33 +0530

    HomePage Buttons Created

--- GuessTheFlag/app/src/main/java/com/example/guesstheflag/MainActivity.kt
+++ GuessTheFlag/app/src/main/java/com/example/guesstheflag/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.guesstheflag
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.guesstheflag.ui.theme.GuessTheFlagTheme
@@ -35,23 +43,57 @@ fun HomePage(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GuessButton(text = "Button 1")
+        Row{
+            Button(onClick = {},modifier=Modifier
+                .size(110.dp),
+//                colors = ButtonDefaults.buttonColors(
+//                    containerColor = Color.Black),
+                border = BorderStroke(3.dp, Color.Black),
+                shape = RoundedCornerShape(20),
+            ) {
+                Text(text = "Guess the Country")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(onClick = {},modifier=Modifier
+                .size(110.dp),
+//                colors = ButtonDefaults.buttonColors(
+//                    containerColor = Color.Magenta),
+                border = BorderStroke(3.dp, Color.Black),
+                shape = RoundedCornerShape(20),
+            ) {
+                Text(text = "Guess - Hints")
+            }
+        }
         Spacer(modifier = Modifier.height(16.dp))
-        GuessButton(text = "Button 2")
-        Spacer(modifier = Modifier.height(16.dp))
-        GuessButton(text = "Button 3")
-        Spacer(modifier = Modifier.height(16.dp))
-        GuessButton(text = "Button 4")
+
+        Row{
+            Button(onClick = {},modifier=Modifier
+                .size(110.dp),
+//                colors = ButtonDefaults.buttonColors(
+//                    containerColor = Color.Magenta),
+                border = BorderStroke(3.dp, Color.Black),
+                shape = RoundedCornerShape(20),
+            ) {
+                Text(text = "Guess the Flag")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(onClick = {},modifier=Modifier
+                .size(110.dp),
+//                colors = ButtonDefaults.buttonColors(
+//                    containerColor = Color.Magenta),
+                border = BorderStroke(3.dp, Color.Black),
+                shape = RoundedCornerShape(20),
+
+            ) {
+                Text(text = "Advanced Level")
+            }
+        }
     }
 
 }
 
-@Composable
-fun GuessButton(text: String) {
-    Button(onClick = { /* Handle button click */ }) {
-        Text(text = text)
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
